462a59b322231397a7caa55da8b76f353524cb99
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-04-28 23:36:55 +0900

    2019.04.28

---
+++ Galaxy-Shooter_new/Assets/2D Galaxy Assets/Game/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyShipPrefab;
+    
+    [SerializeField]
+    private GameObject[] powerups;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
