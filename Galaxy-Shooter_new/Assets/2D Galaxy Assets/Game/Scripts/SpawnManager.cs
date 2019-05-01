b87ca314b924c9467fb409d0de9e8e6a54c4d0e3
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-05-01 15:32:45 +0900

    Powerup Spawning

--- Galaxy-Shooter_new/Assets/2D Galaxy Assets/Game/Scripts/SpawnManager.cs
+++ Galaxy-Shooter_new/Assets/2D Galaxy Assets/Game/Scripts/SpawnManager.cs
@@ -14,14 +14,33 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerupSpawnRoutine());        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator EnemySpawnRoutine()
+    {
+        while(true)
+        {
+            Instantiate(enemyShipPrefab, new Vector3(Random.Range(-7f, 7f),7,0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+
+        }
+    }
+    IEnumerator PowerupSpawnRoutine()
+    {
+        while(true)
+        {
+            int randomPowerup = Random.Range(0,3);
+            Instantiate(powerups[randomPowerup], new Vector3(Random.Range(-7,7), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
     }
 
 
